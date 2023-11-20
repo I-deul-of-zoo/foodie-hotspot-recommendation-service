7989995f7d5aefb018c48fa02bdfe5fe3b03d5c7
tneoeo2 <tneoeo2@gmail.com>
2023-11-21 03:47:57 +0900

    :white_check_mark:Test : 회원가입, 로그인 테스트 코드 추가
    - 회원가입 로그인 테스트 코드 추가
    
    #45

--- auths/tests.py
+++ auths/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class AuthsTests(APITestCase):
+    def setUp(self):
+        # 테스트용 유저 생성
+        self.user_data = {
+            'username': 'testuser',
+            'password': 'testpassword',
+        }
+        self.user = get_user_model().objects.create_user(**self.user_data)
+        
+    def test_signup(self):
+        # 회원가입 테스트
+        url = reverse("auths:signup")
+        data = {
+            'username': 'newuser',
+            'password': 'newpassword',
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['username'], 'newuser')
+
+    def test_jwt_login(self):
+        # JWT 로그인 테스트
+        url =reverse("auths:jwt-login")
+        data = {
+            'username': self.user_data['username'],
+            'password': self.user_data['password'],
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access_token', response.data)
+        self.assertEqual(response.data['username'], self.user_data['username'])
