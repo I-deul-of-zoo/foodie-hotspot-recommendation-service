b567c6ed1f08fd0572928fa561a4567130696f13
tneoeo2 <tneoeo2@gmail.com>
2023-11-21 23:51:56 +0900

    :white_check_mark:Test : 테스트 코드, 리팩토링 테스트 코드 추가
    - 로그인, 회원가입 테스트 코드 추가(pytest)
    
    #45

--- auths/tests.py
+++ auths/tests.py
@@ -1,18 +1,23 @@
-from rest_framework.test import APITestCase
+import pytest, sys, logging
 from rest_framework import status
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
 
 
-class AuthsTests(APITestCase):
-    def setUp(self):
+    
+@pytest.mark.django_db
+class TestAuthsClass():
+    def setup_method(self):
         # 테스트용 유저 생성
         self.user_data = {
             'username': 'testuser',
             'password': 'testpassword',
         }
         self.user = get_user_model().objects.create_user(**self.user_data)
-        
+        self.client = APIClient()
+        logging.info(sys._getframe(0).f_code.co_name)
+
     def test_signup(self):
         # 회원가입 테스트
         url = reverse("auths:signup")
@@ -20,18 +25,21 @@ class AuthsTests(APITestCase):
             'username': 'newuser',
             'password': 'newpassword',
         }
+        logging.info(sys._getframe(0).f_code.co_name)
         response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['username'], 'newuser')
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data
 
     def test_jwt_login(self):
         # JWT 로그인 테스트
-        url =reverse("auths:jwt-login")
+        url = reverse("auths:jwt-login")
         data = {
             'username': self.user_data['username'],
             'password': self.user_data['password'],
         }
+        logging.info(sys._getframe(0).f_code.co_name)
         response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn('access_token', response.data)
-        self.assertEqual(response.data['username'], self.user_data['username'])
+        assert response.status_code == status.HTTP_200_OK
+        assert 'access_token' in response.data
+        assert response.data['username'] == self.user_data['username']
+
