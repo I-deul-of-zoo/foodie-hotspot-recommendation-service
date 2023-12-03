7e55788bd0c3818ae2c98eb8060626ed9a1f48f6
jinsoo <realaqua23@gmail.com>
2023-12-03 16:25:21 +0900

    :bug:Fix : 캐시 설정 없어진 부분 복원

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,19 @@ else:
         }
     }
 
+
+CACHE_TTL = 60 * 1500
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 # CACHE_TTL = 30  # Time to live
 
 # CACHES = {
