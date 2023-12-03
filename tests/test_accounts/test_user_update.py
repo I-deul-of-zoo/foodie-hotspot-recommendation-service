cb058834fac35240aa5262aef3f17fd4ceb37eff
jinsoo <realaqua23@gmail.com>
2023-12-03 16:43:47 +0900

    :fire:Remove : 주석 제거

--- tests/test_accounts/test_user_update.py
+++ tests/test_accounts/test_user_update.py
@@ -6,30 +6,7 @@ from django.core.cache import cache
 
 @pytest.mark.django_db
 def test_user_check(client, access_token):
-    # # 직접 생성하는 경우에는... 잘 안되네
-    # # user = User.objects.create(username='testuser1', password='devpassword&*')
-    
-    # # 유저 회원가입
-    # signup_url = "/api/auth/signup"
-    # data = {"username": "testuser1", "password": "devpassword&*"}
-    # response = client.post(signup_url,
-    #             data=json.dumps(data),
-    #             content_type="application/json")
-    
-    # assert response.status_code == 201
-    
-    # # 유저 토큰
-    # login_url = "/api/auth/jwt-login"
-    # data = {"username": "testuser1", "password": "devpassword&*"}
-    # response = client.post(login_url,
-    #             data=json.dumps(data),
-    #             content_type="application/json")
-    
-    # assert response.status_code == 200
-    # assert response.json().get("access_token") != None
 
-    # # 초기 유저정보 확인
-    # access_token = response.json().get("access_token")
     url = "/api/account/"
     response = client.get(url,
                         headers={"Authorization": f"Bearer {access_token}"},
@@ -44,7 +21,7 @@ def test_user_check(client, access_token):
     assert longitude == ""
     assert is_recommend == False
 
-def test_user_update(client, access_token):  
+def test_user_update(client, access_token, access_db):  
     # 유저 정보 변경 후 체크
     expected = {
         "latitude": "100.1234",
